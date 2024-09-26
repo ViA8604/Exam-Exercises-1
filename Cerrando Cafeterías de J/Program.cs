16169490d3c0b5cca7dbe5d7a9d00ba69429efc8
Anthony <55062795+anthonycgarcia2003@users.noreply.github.com>
2024-09-26 12:51:25 -0400

    My solution

--- Cerrando Cafeterías de J/Program.cs
+++ Cerrando Cafeterías de J/Program.cs
@@ -1,57 +1,111 @@
 ﻿
 
-public class Program
+using System.Reflection.Metadata;
+
+ public class Program
 {
     public static void Main()
     {
 
     }
 
-    public static void CafeteriasACerrar(int[,] capacidad, int[,] real)
+    public static int CafeteriasACerrar(int[,] capacidad, int[,] real)
     {
-        int[,] disponibilidad = GetDisponibilidad(capacidad, real);
-        bool[] mascara = new bool[capacidad.GetLength(1)];
-
-        int totalCaf = 0;
-
-        CerrarCafeterias(0, capacidad, real, disponibilidad, mascara, ref totalCaf);
+        int maxClose=0;
+        CloseCafeterias(capacidad,real,ref maxClose,0,FalseArray(new bool[capacidad.GetLength(1)]),FalseArray(new bool[capacidad.GetLength(1)]));
+        return maxClose;
+        
     }
-
-    private static void CerrarCafeterias(int columna, int[,] capacidad, int[,] real, int[,] disponibilidad, bool[] mascara, ref int totalCaf)
+    public static void CloseCafeterias(int[,] capacidad, int[,] real,ref int maxClose,int closes,bool[]TryClose,bool[]ClosedCafeterias)
     {
-
-    }
-
-    private static bool IntentarCerrar(int cafeteria, int[,] real, int[,] disponibilidad)
-    {
-        for (int i = 0; i < real.GetLength(0); i++)
+        if(AllVisitsCafeterias(TryClose))
         {
-            for (int j = 0; j < real.GetLength(1); j++)
+            if(closes>maxClose)
             {
-                if (j == cafeteria)
+                maxClose=closes;
+            }
+            return;
+        }
+        for(int c=0;c<TryClose.Length;c++)
+        {
+            if(!TryClose[c])
+            {
+                TryClose[c]=true;
+                int[]AvariablesCaferias=Close(capacidad,real,ClosedCafeterias,c);
+                if(AvariablesCaferias.Length!=0)
                 {
-                    continue;
+                    ClosedCafeterias[c]=true;
+                    closes++;
+                    for(int i=0;i<AvariablesCaferias.Length;i++)
+                    {
+                        MergeColumns(capacidad,c,AvariablesCaferias[i]);
+                        CloseCafeterias(capacidad,real,ref maxClose,closes,TryClose,ClosedCafeterias);
+                        DesMerge(capacidad,c,AvariablesCaferias[i]);
+                    }
+                    closes--;
+                    ClosedCafeterias[c]=false;
                 }
-                else
+                TryClose[c]=false;
+            }
+        }
+    }
+    static bool[] FalseArray(bool[] array)
+    {
+        for(int i=0;i<array.Length;i++)
+        {
+            array[i]=false;
+        }
+        return array;
+    }
+    static int[] Close(int[,] capacidad, int[,] real,bool[] ClosedCafeterias,int cafeteria)
+    {
+        List<int>AvariablesCafeterias=new List<int>();
+        for(int i=0;i<ClosedCafeterias.Length;i++)
+        {
+            if(i!=cafeteria && !ClosedCafeterias[i])
+            {
+                if(CanClose(i))
                 {
-
+                    AvariablesCafeterias.Add(i);
                 }
+            }
+        }
+        return AvariablesCafeterias.ToArray();
+        bool CanClose(int i)
+        {
+            for(int r=0;r<capacidad.GetLength(0);r++)
+            {
+                    if(capacidad[i,r]+capacidad[cafeteria,r]>real[i,r])
+                    {
+                       return false;//hay q ver si pincha
+                    }
+            }
+            return true;
+        }
+    }
+    static void DesMerge(int[,] capacidad,int CloseCafeteria,int OpenCafeteria)
+    {
+        for(int i=0;i<capacidad.GetLength(0);i++)
+        {
+            capacidad[i,OpenCafeteria]-=capacidad[i,CloseCafeteria];
+        }
+    }
+    static void MergeColumns(int[,] capacidad,int CloseCafeteria,int OpenCafeteria)
+    {
+        for(int i=0;i<capacidad.GetLength(0);i++)
+        {
+            capacidad[i,OpenCafeteria]+=capacidad[i,CloseCafeteria];
+        }
+    }
+    static bool AllVisitsCafeterias(bool[]VisitCafeterias)
+    {
+        foreach(bool visited in VisitCafeterias)
+        {
+            if(!visited)
+            {
+                return false;
             }
         }
         return true;
-    }
-
-    private static int[,] GetDisponibilidad(int[,] capacidad, int[,] real)
-    {
-        int[,] disponibilidad = new int[capacidad.GetLength(0), capacidad.GetLength(1)];
-
-        for (int i = 0; i < capacidad.GetLength(0); i++)
-        {
-            for (int j = 0; j < capacidad.GetLength(1); j++)
-            {
-                disponibilidad[i, j] = capacidad[i, j] - real[i, j];
-            }
-        }
-        return disponibilidad;
     }
 }
