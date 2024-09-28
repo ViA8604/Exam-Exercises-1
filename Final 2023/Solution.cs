88f78c9fec4b22dd34d2073c584d0a278376693d
Anthony <55062795+anthonycgarcia2003@users.noreply.github.com>
2024-09-27 20:21:37 -0400

    Add final 2023

---
+++ Final 2023/Solution.cs
@@ -0,0 +1,8 @@
+public static class Solution
+{
+    public static int GetRoutes(Map map, int n)
+    {
+        // Elimine la siguiente línea y ponga aquí su código
+       throw new Exception();
+    }
+}
