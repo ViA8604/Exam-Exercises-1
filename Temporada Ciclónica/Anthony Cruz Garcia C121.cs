506c01414e829a5fb2d358c394393a970ebdaca3
Anthony <55062795+anthonycgarcia2003@users.noreply.github.com>
2024-10-03 21:39:48 -0400

    Ojala lo hubiera hecho asi en la prueba xd

---
+++ Temporada Ciclónica/Anthony Cruz Garcia C121.cs
@@ -0,0 +1,90 @@
+public static class Examen
+{
+    public static int MinEstudiantesAvisar(bool[,] amigos, int k)
+    {
+        int MinStudents = int.MaxValue;
+        for (int i = 0; i <= k; i++)
+        {
+            if (MinStudents == 1)
+            {
+                return 1;
+            }
+            AlertStudents(new bool[amigos.GetLength(0)], ref MinStudents, 0, amigos, i,new bool[amigos.GetLength(0)]);
+        }
+
+        return MinStudents;
+    }
+    static void AlertStudents(bool[] studentsAlert, ref int MinStudents, int students, bool[,] friends, int profundity,bool[]DecanoAlert)
+    {
+
+        if (AllStudentsAlert(studentsAlert))//caso base
+        {
+            if (students < MinStudents)
+            {
+                MinStudents = students;
+            }
+            return;
+        }
+
+        if (students > MinStudents)//poda
+        {
+            return;
+        }
+        for(int i=0;i<studentsAlert.Length;i++)
+        {
+            if(!DecanoAlert[i])
+            {
+                DecanoAlert[i]=true;
+                AlertStudents(Alert(studentsAlert,friends,i,profundity),ref MinStudents,students+1,friends,profundity,DecanoAlert);
+                DecanoAlert[i]=false;
+            }
+        }
+    }
+    static bool[] Alert(bool[] studentsAlert, bool[,] friends, int student, int profundity)
+    {
+        bool[]Mask=CopyMask(studentsAlert);
+        Mask[student] = true;
+        for (int a = 0; a < profundity; a++)
+        {
+            for (int i = 0; i < studentsAlert.Length; i++)
+            {
+                if (friends[i, student])
+                {
+                    Mark(Mask,friends,i,profundity-1);
+                }
+            }
+        }
+        return Mask;
+    }
+    static void Mark(bool[] studentsAlert, bool[,] friends, int student, int profundity)
+    {
+        studentsAlert[student]=true;
+        for (int a = 0; a < profundity; a++)
+        {
+            for (int i = 0; i < studentsAlert.Length; i++)
+            {
+                if (friends[i, student])
+                {
+                    Mark(studentsAlert,friends,i,profundity-1);
+                }
+            }
+        }
+    }
+    static bool AllStudentsAlert(bool[] studentsAlert)
+    {
+        foreach (bool IsAlert in studentsAlert)
+        {
+            if (!IsAlert)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static bool[] CopyMask(bool[] source)
+    {
+        bool[] NewMask = new bool[source.Length];
+        Array.Copy(source, NewMask, source.Length);
+        return NewMask;
+    }
+}
---
+++ Temporada Ciclónica/Program.cs
@@ -0,0 +1,24 @@
+﻿
+bool[,] Amigos =
+{
+    { false, true,  true,  false, false, false, false, false, false, },
+    { true,  false, false, true,  false, true,  false, true,  false, },
+    { true,  false, false, false, true,  false, true,  false, true, },
+    { false, true,  false, false, false, false, false, false, false, },
+    { false, false, true,  false, false, false, false, false, false, },
+    { false, true,  false, false, false, false, false, false, false, },
+    { false, false, true,  false, false, false, false, false, false, },
+    { false, true,  false, false, false, false, false, false, false, },
+    { false, false, true,  false, false, false, false, false, false, },
+};
+
+int[] Expected = { 9, 2, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+for(int k = 0; k < 10; k++)
+{
+    var result = Examen.MinEstudiantesAvisar(Amigos, k);
+    if (result != Expected[k]) Console.WriteLine($"Test #{k + 1}: Se esperaba {Expected[k]}, se obtuvo {result}");
+    else Console.WriteLine($"Test #{k + 1}: S={result}. Correcto!");
+}
+
+
