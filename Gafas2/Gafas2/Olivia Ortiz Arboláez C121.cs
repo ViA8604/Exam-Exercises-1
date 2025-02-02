987071bde8c08e89a7df9c6faed80484ce9b1b6a
ViA8604 <rolikvia8@gmail.com>
2025-02-02 18:11:58 -0500

    Gafas2 commented

---
+++ Gafas2/Gafas2/Olivia Ortiz Arboláez C121.cs
@@ -0,0 +1,67 @@
+﻿namespace Weboo.Examen;
+
+public static class Solution
+{
+    public static int Solve(int[,] map, int capacity, int origin)
+    {
+        int minCombustible = int.MaxValue;
+        bool[] visitedCities = new bool[map.GetLength(0)];
+        visitedCities[origin] = true; //maarca la ciudad de origen como visitada
+
+        AgenciaNoel(map, visitedCities, capacity, origin, origin, capacity, 0, ref minCombustible);
+        if(minCombustible == int.MaxValue)  //si no se pudo llegar a todas las ciudades
+        {
+            return -1;
+        }
+        return minCombustible;
+    }
+
+    public static void AgenciaNoel(int[,] map, bool[] visitedCities, int capacity, int origin, int actualCity, int combustibleActual, int cantCombustibleUsado, ref int MinTotal)
+    {
+        if (cantCombustibleUsado >= MinTotal)   //poda
+        {
+            return;
+        }
+        if(actualCity == origin) //rellenar el tanke
+        {
+            combustibleActual = capacity;
+        }
+
+        //Caso Base
+        if (AllVisited(origin, visitedCities) && actualCity == origin) //si ya se visitaron todas las ciudades
+        {
+            MinTotal = Math.Min(cantCombustibleUsado, MinTotal);
+            return;
+        }
+
+        //Caso Recursivo
+        for (int i = 0; i < visitedCities.Length; i++)
+        {
+            if (!visitedCities[i] && map[actualCity, i] <= combustibleActual)
+            {
+                //visitar la ciudad i
+                visitedCities[i] = true;
+                AgenciaNoel(map, visitedCities, capacity, origin, i, combustibleActual - map[actualCity, i], cantCombustibleUsado + map[actualCity, i], ref MinTotal);
+                visitedCities[i] = false;
+            }
+        }
+
+        if (actualCity != origin && map[actualCity, origin] <= combustibleActual)
+        {
+            //volver a la ciudad de origen
+            AgenciaNoel(map, visitedCities, capacity, origin, origin, capacity, cantCombustibleUsado + map[actualCity, origin], ref MinTotal);
+        }
+    }
+
+    static bool AllVisited (int origin, bool [] visitedCities)
+    {
+        for(int i = 0; i < visitedCities.Length; i++)
+        {
+            if(i != origin && !visitedCities[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Gafas2/Gafas2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace Weboo.Examen;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[,] map1 =
+        {
+            { 0, 1, 1, 1 },
+            { 1, 0, 1, 1 },
+            { 1, 1, 0, 1 },
+            { 1, 1, 1, 0 }
+        };
+        
+       int result = Solution.Solve(map1, 1, 0);
+        Debug.Assert(result == -1);
+        
+        int result1 = Solution.Solve(map1, 2, 0);
+        Debug.Assert(result1 == 6);
+        
+        int result2 = Solution.Solve(map1, 3, 0);
+        Debug.Assert(result2 == 5);
+        
+        int result3 = Solution.Solve(map1, 4, 0);
+        Debug.Assert(result3 == 4);
+        
+        int[,] map2 =
+        {
+            { 0, 2, 4, 6 },
+            { 7, 0, 2, 4 },
+            { 9, 9, 0, 5 },
+            { 7, 4, 6, 0 }
+        };
+        int result4 = Solution.Solve(map2, 14, 1);
+        Debug.Assert(result4 == 20);
+        
+
+        int[,] map3 =
+        {
+            { 0, 4, 4, 3, 8 },
+            { 2, 0, 2, 5, 9 },
+            { 7, 5, 0, 4, 7 },
+            { 3, 1, 3, 0, 5 },
+            { 6, 4, 6, 8, 0 }
+        };
+
+        int result5 = Solution.Solve(map3, 17, 0);
+        Debug.Assert(result5 == 23);
+
+        int result6 = Solution.Solve(map3, 15, 0);
+        Debug.Assert(result6 == 25);
+
+        int result7 = Solution.Solve(map3, 13, 0);
+        Debug.Assert(result7 == -1);
+
+        Console.WriteLine("All tests passed!!!");
+    }
+}
